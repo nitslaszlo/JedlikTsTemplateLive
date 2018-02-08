ed573ec052bf14f8f9873f1e84955da44a0f1521
Nits László <nits@jedlik.helyi>
2018-02-08 11:21:05 +0100

    First up

---
+++ ts/app.ts
@@ -0,0 +1,16 @@
+namespace Jedlik {
+
+    export class MyApplication  {
+
+        constructor() {
+            const element = document.createElement("h1");
+            element.innerHTML = "Hello World!";
+            element.setAttribute("style", "color:Red");
+            document.body.appendChild(element);
+        }
+    }
+}
+
+window.onload = () => {
+    const app = new Jedlik.MyApplication();
+};
